338b810f56645273f674448fa3298646b8870d28
itsrumana <itsrumanabegum@gmail.com>
2024-07-12 22:03:49 +0530

    updated minor features

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function hideSideBar(){
 }
 
 var typed= new Typed(".auto-typed",{
-    strings:["My name is Rumana Begum.","I am a programmer.","Also enthusiast learner.","You can call me Rumana.","I am a Developer.","I am a Designer.","My mom calls me Ruma."],
+    strings:["My name is Rumana Begum.","I am a programmer.","Also enthusiast learner.","You can call me Rumana.","I am a Developer.","I am a Designer."],
     typeSpeed:120,
     backSpeed:50,
     loop:true
